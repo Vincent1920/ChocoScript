c5d62ecf71653e44ccbe3fbb95e1be984e67d40b
Vincent1920 <vincentluhulima6010@gmail.com>
2026-03-09 23:04:29 +0700

    first commit

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -7,3 +7,12 @@ import Navbar from './components/Navbar.vue'
   <Navbar />
   <RouterView />
 </template>
+
+<style>
+/* Reset dasar agar layout penuh */
+body, html {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+</style>
